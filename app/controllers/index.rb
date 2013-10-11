807ed7d6c316caf1d6ce814b5e8535317869d07c
siciliana <siciliana@gmail.com>
2013-10-11 13:26:59 -0500

    testing

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,4 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
@@ -9,14 +8,19 @@ end
 #========== POST =========
 
 post '/signup' do
+	# create a new user and save to db 
+
 	:profile
 end
 
 post '/login' do
+	# searches database for user info by email
 	:profile
 end 
 
 post '/game/:game_id/:card_id' do
+	# if game is not finished, continue iterating through cards and display game board view
 	:game_board
+	# if game is finished, display game results view
 	:game_results 
 end
