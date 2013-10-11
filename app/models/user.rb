f3280c539422ec66f2572a081314f72c4773f98a
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-11 14:20:34 -0500

    set up bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+  require 'bcrypt'
+
+  validates :email, presence: true, uniqueness: true, case_sensitive: false 
+  validates :password, presence: true
+
+  has_secure_password 
 	has_many :rounds
 	has_many :guesses, through: :rounds 
 end
