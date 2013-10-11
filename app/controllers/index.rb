f9525ce9e79b95ca293be604ff2efee4b7439e0f
siciliana <siciliana@gmail.com>
2013-10-11 12:51:31 -0500

    created skeletal framework of controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,21 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/logout' do 
+	:index
+end 
+#========== POST =========
+
+post '/signup' do
+	:profile
+end
+
+post '/login' do
+	:profile
+end 
+
+post '/game/:game_id/:card_id' do
+	:game_board
+	:game_results 
+end
