dce755498e2f6f3cdeff3e1f5c4a8c1e08d7d459
Apprentice <apprentice@dbc-chicago033.local>
2013-10-11 15:23:31 -0500

    setup login and signup routes and view

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,23 +6,38 @@ get '/logout' do
 	session.clear 
 	redirect '/' 
 end 
+
+get '/profile' do 
+	@user = current_user
+	@decks = Deck.all 
+	erb :profile 
+end
+
 #========== POST =========
 
 post '/signup' do
 	# create a new user and save to db 
-
-	:profile
+	@user = User.create(params[:user])
+	redirect '/'
 end
+
 
 post '/login' do
 	# searches database for user info by email	
 	# establish session through user_id
-	:profile
+	@user = User.find_by_email(params[:user][:email])
+	if @user
+		session[:user_id] = @user.id 
+		redirect '/profile'
+	else 
+		session[:error] = "invalid login"
+		redirect '/'
+	end 
 end 
 
 post '/game/:game_id/:card_id' do
 	# if game is not finished, continue iterating through cards and display game board view
-	erb :game
+	redirect '/game'
 	# if game is finished, display game results view
 	# :game_results 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  require 'bcrypt'
+  include BCrypt
 
   validates :email, presence: true, uniqueness: true, case_sensitive: false 
   validates :password, presence: true
